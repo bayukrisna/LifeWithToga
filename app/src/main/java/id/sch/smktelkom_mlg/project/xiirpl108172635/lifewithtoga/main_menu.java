d4b13cf79f64ddd9ff8d631ac863dfcb9efbc20b
yobelchris <yobelchris@gmail.com>
2016-11-16 22:57:19 +0700

    Revisi Menu Utama 2

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl108172635/lifewithtoga/main_menu.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl108172635/lifewithtoga/main_menu.java
@@ -1,13 +1,56 @@
 package id.sch.smktelkom_mlg.project.xiirpl108172635.lifewithtoga;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class main_menu extends AppCompatActivity {
+
+    public static final String ANGT = "Angt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+
+        findViewById(R.id.body).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hasilPindah("badan");
+            }
+        });
+        findViewById(R.id.headUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hasilPindah("kepala");
+            }
+        });
+        findViewById(R.id.handLeft).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hasilPindah("tangan");
+            }
+        });
+        findViewById(R.id.handRight).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hasilPindah("tangan");
+            }
+        });
+        findViewById(R.id.legUnder).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hasilPindah("kaki");
+            }
+        });
+    }
+
+    private void hasilPindah(String agt) {
+        Intent intent = new Intent(main_menu.this, MainActivity.class);
+        intent.putExtra(ANGT, agt);
+
+        startActivity(intent);
     }
 }
