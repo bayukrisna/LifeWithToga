8f60cfd7f206e69689a875365d22003d56d817d6
Bayu Krisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2016-11-17 16:20:05 +0700

    Modifikasi Array

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl108172635/lifewithtoga/DetailActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl108172635/lifewithtoga/DetailActivity.java
@@ -2,8 +2,6 @@ package id.sch.smktelkom_mlg.project.xiirpl108172635.lifewithtoga;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -35,17 +33,8 @@ public class DetailActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
-
             }
         });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
